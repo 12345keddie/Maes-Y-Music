0bfcb367d2aea06edbfa77be9ff99fbdc8e94ca9
Leo Keddie <leonardokeddie@gmail.com>
2025-10-09 06:39:45 +0000

    I added Rick Astley - Never Gonna Give You Up (Official Video) (4K Remas

--- src/lib/videos.ts
+++ src/lib/videos.ts
@@ -14,51 +14,10 @@ const getThumbnail = (id: string) =>
 
 export const videos: Video[] = [
   {
-    id: 'vid1',
-    title: 'Cosmic Drift',
-    artist: 'Galaxy Runners',
+    id: 'rick-astley',
+    title: 'Never Gonna Give You Up',
+    artist: 'Rick Astley',
     thumbnailUrl: getThumbnail('video-thumb-1'),
-    videoUrl:
-      'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4',
-  },
-  {
-    id: 'vid2',
-    title: 'Midnight Drive',
-    artist: 'Night Crawlers',
-    thumbnailUrl: getThumbnail('video-thumb-2'),
-    videoUrl:
-      'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4',
-  },
-  {
-    id: 'vid3',
-    title: 'Neon Bloom',
-    artist: 'Cyberflora',
-    thumbnailUrl: getThumbnail('video-thumb-3'),
-    videoUrl:
-      'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4',
-  },
-  {
-    id: 'vid4',
-    title: 'Starlight Echoes',
-    artist: 'Astro Beats',
-    thumbnailUrl: getThumbnail('video-thumb-4'),
-    videoUrl:
-      'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4',
-  },
-  {
-    id: 'vid5',
-    title: 'City Lights',
-    artist: 'Urban Pulse',
-    thumbnailUrl: getThumbnail('video-thumb-5'),
-    videoUrl:
-      'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
-  },
-  {
-    id: 'vid6',
-    title: 'Quantum Leap',
-    artist: 'Dimension Drifters',
-    thumbnailUrl: getThumbnail('video-thumb-6'),
-    videoUrl:
-      'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerMeltdowns.mp4',
+    videoUrl: '/Rick Astley - Never Gonna Give You Up (Official Video) (4K Remaster).mp4',
   },
 ];
