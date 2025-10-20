98ae4e6c4eefd1c1f6fbd83c7970e7e440690192
Leo Keddie <leonardokeddie@gmail.com>
2025-10-20 19:06:48 +0000

    20:02:03.754 Running build in Portland, USA (West) – pdx1 20:02:03.755 B

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -7,29 +7,27 @@ import { getFirestore } from 'firebase/firestore'
 
 // IMPORTANT: DO NOT MODIFY THIS FUNCTION
 export function initializeFirebase() {
-  if (!getApps().length) {
-    // Important! initializeApp() is called without any arguments because Firebase App Hosting
-    // integrates with the initializeApp() function to provide the environment variables needed to
-    // populate the FirebaseOptions in production. It is critical that we attempt to call initializeApp()
-    // without arguments.
-    let firebaseApp;
-    try {
-      // Attempt to initialize via Firebase App Hosting environment variables
-      firebaseApp = initializeApp();
-    } catch (e) {
-      // Only warn in production because it's normal to use the firebaseConfig to initialize
-      // during development
-      if (process.env.NODE_ENV === "production") {
-        console.warn('Automatic initialization failed. Falling back to firebase config object.', e);
-      }
-      firebaseApp = initializeApp(firebaseConfig);
-    }
-
-    return getSdks(firebaseApp);
+  if (getApps().length) {
+    return getSdks(getApp());
   }
 
-  // If already initialized, return the SDKs with the already initialized App
-  return getSdks(getApp());
+  // When not in a browser, or when automatic initialization is not supported (e.g. Vercel),
+  // we must initialize with the config object.
+  if (typeof window === 'undefined' || process.env.NODE_ENV === 'test') {
+      const firebaseApp = initializeApp(firebaseConfig);
+      return getSdks(firebaseApp);
+  }
+
+  // In the browser, first try automatic initialization for Firebase Hosting.
+  try {
+    const firebaseApp = initializeApp();
+    return getSdks(firebaseApp);
+  } catch (e) {
+    console.warn('Automatic Firebase initialization failed. Falling back to firebase config object.', e);
+    // If that fails, fall back to the config object.
+    const firebaseApp = initializeApp(firebaseConfig);
+    return getSdks(firebaseApp);
+  }
 }
 
 export function getSdks(firebaseApp: FirebaseApp) {
